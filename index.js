3c0d83b7b114b124e48531a97d4f94e1ca176c90
leduckhuong <leduckhuong@kali.kali.org>
2024-07-27 09:46:15 +0700

    add

--- index.js
+++ index.js
@@ -20,21 +20,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const generateToken = payload => {
+const generateToken = async ( user ) => {
     const tokenData = {
-        id: payload._id
+        id: user._id.toString()
     }
     const accessToken = jwt.sign(tokenData, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '15m'
+        expiresIn: process.env.ACCESS_TOKEN_TIME
     });
     const refreshToken = jwt.sign(tokenData, process.env.REFRESH_TOKEN_SECRET, {
-        expiresIn: '30d'
+        expiresIn: process.env.REFRESH_TOKEN_TIME
     });
     return {accessToken, refreshToken};
 }
 
-const updateRefreshToken = (id, refreshToken) => {
-    UserModel.findByIdAndUpdate(id, { refreshToken });
+const updateRefreshToken = async (id, refreshToken) => {
+    return await UserModel.findByIdAndUpdate(id, { refreshToken });
 }
 
 const compare = (passw, hashpw) => {
@@ -46,81 +46,90 @@ app.get('/', (req, res) => {
     res.send('ok');
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
+    console.log('Is register');
+    const data = req.body;
+    if (!data.email || !data.password) {
+        return res.status(400).json({ message: 'Invalid request data' });
+    }
     const passw = req.body.password;
     const hashpw = cryptojs.SHA256(passw).toString();
-    const data = {
+    const userData = {
         email: req.body.email,
         hashpw
     }
-    UserModel.create(data)
-        .then(user => {
-            const  { accessToken, refreshToken } = generateToken(user);
-            updateRefreshToken(user._id.toString(), refreshToken);
-            res.json({
-                accessToken
-            });
-        })
-        .catch(error => {
-            console.log('Error: ', error)
-            res.sendStatus(409);
-        })
-})
-
-app.post('/login', (req, res) => {
-    const data = req.body;
-    UserModel.findOne({ email: data.email })
-        .then(user => {
-            if(!compare(data.password, user.hashpw)) return res.sendStatus(401);
-            const { accessToken, refreshToken } = generateToken(user);
-            updateRefreshToken(user._id.toString(), refreshToken);
-            res.json({
-                accessToken
-            });
-        })
-        .catch(error =>{
-            console.log("Error: ", error);
-            res.sendStatus(401);
-        });
-})
-
-app.post('/token', (req, res) => {
-    const refreshToken = req.body.refreshToken;
-    if (!refreshToken) return res.sendStatus(401);
     try {
-        const refreshData = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-        UserModel.findById(refreshData.id)
-            .then(user => {
-                const { accessToken, refreshToken } = generateToken(user);
-                updateRefreshToken(user._id.toString(), refreshToken);
-                res.json({
-                    accessToken
-                });
-            })
-            .catch(error =>{
-                console.log("Error: ", error);
-                res.sendStatus(401);
-            });
+        const user = await UserModel.create(userData);
+        const  { accessToken, refreshToken } = await generateToken(user);
+        await updateRefreshToken(user._id.toString(), refreshToken);
+        const uid = user._id.toString();
+        return res.status(201).json({uid, accessToken});
     } catch (error) {
-        console.log(error);
-        res.sendStatus(403);
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ message: 'Invalid request data', error: error.message });
+        } else if (error.code === 11000) {
+            console.log('1100');
+            return res.status(409).json({ message: 'Conflict: Duplicate data' });
+        } else {
+            return res.status(500).json({ message: 'Internal server error', error: error.message });
+        }
     }
 })
 
-app.get('/data', verifyToken, (req, res) => {
-    UserModel.findById(req.userId)
-        .then(response => {
-            res.json(response);
-        })
-        .catch(error => {
-            console.log(error);
-            res.sendStatus(403);
-        })
+
+app.post('/login', async (req, res) => {
+    console.log('Is login');
+    const data = req.body;
+    if (!data.email || !data.password) return res.status(400).json({ message: 'Invalid request data' });
+    try {
+        const user = await UserModel.findOne({ email: data.email });
+        if (!user) return res.status(401).json({ message: 'Unauthorized' });
+        if(!compare(data.password, user.hashpw)) return res.status(401).json({ message: 'Unauthorized' });
+        const  { accessToken, refreshToken } = await generateToken(user);
+        await updateRefreshToken(user._id.toString(), refreshToken);
+        const uid = user._id.toString();
+        return res.status(200).json({ uid, accessToken });
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
-app.delete('/logout', verifyToken, (req, res) => {
-    UserModel.findByIdAndUpdate(req.userId, { refreshToken: null });
-    res.sendStatus(204);
+app.post('/token', async (req, res) => {
+    console.log('Is token');
+    const rft = req.body.refreshToken;
+    if (!rft) return res.status(401).json({ message: 'Unauthorized: No refresh token provided' });
+    try {
+        const rftData = jwt.verify(rft, process.env.REFRESH_TOKEN_SECRET);
+        const user = await UserModel.findById(rftData.id);
+        if(!user) return res.status(404).json({ message: 'User not found' });
+        const { accessToken, refreshToken } = await generateToken(user);
+        await updateRefreshToken(user._id.toString(), refreshToken);
+        return res.status(200).json({ accessToken });
+    } catch (error) {
+        if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') return res.status(403).json({ message: 'Forbidden: Invalid or expired refresh token' });
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+app.get('/data', verifyToken, async (req, res) => {
+    console.log('Is data');
+    try {
+        const user = await UserModel.findById(req.userId);
+        if (!user) return res.status(404).json({ message: 'Resource not found' });
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+})
+
+app.delete('/logout', verifyToken, async (req, res) => {
+    console.log('Is logout');
+    try {
+        await UserModel.findByIdAndUpdate(req.userId, { refreshToken: null });
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
 })
 
 app.listen(port, () => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,19 @@ const jwt = require('jsonwebtoken');
 exports.verifyToken = (req, res, next) => {
     const authHeader = req.header('Authorization');
     const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.status(401).json({ message: 'Access token is missing' });
     try {
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         req.userId = decoded.id;
         next();
     } catch (error) {
-        console.log(error);
-        return res.sendStatus(403);
+        console.log('Is verify');
+        if (error.name === 'TokenExpiredError') {
+            return res.status(403).json({ message: 'Access token has expired' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(403).json({ message: 'Invalid access token' });
+        } else {
+            return res.status(403).json({ message: 'Forbidden' });
+        }
     }
 }
